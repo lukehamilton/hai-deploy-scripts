efc6cd5ad43fbb2050d9f8c7fec6bb54abc3c3c8
Luke Hamilton <lukehamiltonmail@gmail.com>
2023-01-26 22:39:03 -0500

    Parent script

---
+++ script/BaseDeploy.t.sol
@@ -0,0 +1,10 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.6.7;
+
+import "forge-std/Script.sol";
+
+contract BaseDeploy is Script {
+    function run() external {
+        // Deploy comparison helper
+    }
+}
