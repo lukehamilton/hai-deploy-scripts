b1bf5ac5b78ef26b7eb0dd00e5fd88a6db0de10c
Luke Hamilton <lukehamiltonmail@gmail.com>
2023-01-30 20:49:34 -0500

    wip

--- install-deps.sh
+++ install-deps.sh
@@ -2,4 +2,5 @@ forge install lukehamilton/ds-compare
 forge install lukehamilton/ds-value
 forge install lukehamilton/geb-tx-manager
 forge install lukehamilton/geb-deploy
+forge install lukehamilton/geb-fsm
 forge install lukehamilton/geb-uniswap-median
